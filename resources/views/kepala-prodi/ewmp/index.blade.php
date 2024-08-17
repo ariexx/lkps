11120a493458fde86979b4312c2627d37adca762
Muhammad Arief <mgi.official1@gmail.com>
2024-08-17 17:56:21 +0700

    refactor: EWMP show

--- resources/views/kepala-prodi/ewmp/index.blade.php
+++ resources/views/kepala-prodi/ewmp/index.blade.php
@@ -13,9 +13,14 @@
         </x-create-button>
         <x-adminlte-datatable id="table1" :config="$config" striped hoverable with-buttons :heads="$config['heads']">
             <tfoot>
-            <th colspan="9">Rata Rata</th>
-            <th>{{$config["rata_rata_jumlah"]}}</th>
-            <th>{{$config["rata_rata_sks"]}}</th>
+            <tr>
+                <th>Rata Rata SKS:</th>
+                <th>{{$config["rata_rata_sks"]}}</th>
+            </tr>
+            <tr>
+                <th>Rata Rata Jumlah:</th>
+                <th>{{$config["rata_rata_jumlah"]}}</th>
+            </tr>
             </tfoot>
         </x-adminlte-datatable>
     </x-adminlte-card>
