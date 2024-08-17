827accb1f723bbf989fbb646480a0268921424d0
Muhammad Arief <mgi.official1@gmail.com>
2024-08-17 17:44:40 +0700

    refactor: EWMP show

--- resources/views/components/buttons.blade.php
+++ resources/views/components/buttons.blade.php
@@ -1,32 +1,33 @@
-<!-- resources/views/components/buttons.blade.php -->
 @php
-$role = Auth::user()->role ?? user()->role;
-//const canEdit = [\App\Models\User::prodi, \App\Models\User::adminprodi];
-//const canDelete = [\App\Models\User::prodi];
-//const canApprove = [\App\Models\User::prodi];
+    $role = Auth::user()->role ?? user()->role;
+    $canEdit = ["prodi", "admin_prodi"];
+    $canDelete = ["prodi"];
+    $canApprove = ["prodi"];
 @endphp
+
 <div class="d-flex justify-content-between">
-    @if(in_array($role, ["prodi", "admin_prodi"]))
-        @php Log::info($role) @endphp
-        <a href="{{$routeEdit}}" class="btn btn-primary mr-2">Edit</a>
-        @if($role === "prodi")
-        <form action="{{$routeDelete}}" method="post" class="d-inline mr-2">
+    @if(in_array($role, $canEdit))
+        <a href="{{ $routeEdit }}" class="btn btn-primary mr-2">Edit</a>
+    @endif
+
+    @if(in_array($role, $canDelete))
+        <form action="{{ $routeDelete }}" method="post" class="d-inline mr-2">
             @csrf
             @method('DELETE')
             <button type="submit" class="btn btn-danger">Hapus</button>
         </form>
-            @if($isApproved === STATUS_PENDING)
-                <form action="{{$routeApprove ?? "#"}}" method="post" class="d-inline mr-2">
-                    @csrf
-                    @method('PUT')
-                    <button type="submit" class="btn btn-success">Setujui</button>
-                </form>
-                <form action="{{$routeReject ?? "#"}}" method="post" class="d-inline mr-2">
-                    @csrf
-                    @method('PUT')
-                    <button type="submit" class="btn btn-danger">Tolak</button>
-                </form>
-            @endif
-        @endif
+    @endif
+
+    @if(in_array($role, $canApprove) && $isApproved === 0 || $isApproved == STATUS_PENDING)
+        <form action="{{ $routeApprove ?? '#' }}" method="post" class="d-inline mr-2">
+            @csrf
+            @method('PUT')
+            <button type="submit" class="btn btn-success">Setujui</button>
+        </form>
+        <form action="{{ $routeReject ?? '#' }}" method="post" class="d-inline mr-2">
+            @csrf
+            @method('PUT')
+            <button type="submit" class="btn btn-danger">Tolak</button>
+        </form>
     @endif
 </div>
