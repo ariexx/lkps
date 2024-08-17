1954f7892c9700732cb89aa8324a9afc6e0a0231
Muhammad Arief <mgi.official1@gmail.com>
2024-08-17 16:32:54 +0700

    refactor: compoennts

--- resources/views/components/buttons.blade.php
+++ resources/views/components/buttons.blade.php
@@ -1,16 +1,19 @@
 <!-- resources/views/components/buttons.blade.php -->
 @php
 $role = Auth::user()->role ?? user()->role;
+const canEdit = [\App\Models\User::prodi, \App\Models\User::adminprodi];
+const canDelete = [\App\Models\User::prodi];
+const canApprove = [\App\Models\User::prodi];
 @endphp
 <div class="d-flex justify-content-between">
-    @if(in_array($role, App\Models\User::canEdit) && in_array($role, App\Models\User::canDelete))
+    @if(in_array($role, canEdit) && in_array($role, canDelete))
         <a href="{{$routeEdit}}" class="btn btn-primary mr-2">Edit</a>
         <form action="{{$routeDelete}}" method="post" class="d-inline mr-2">
             @csrf
             @method('DELETE')
             <button type="submit" class="btn btn-danger">Hapus</button>
         </form>
-        @if(in_array($role, App\Models\User::canApprove))
+        @if(in_array($role, canApprove))
             @if($isApproved === STATUS_PENDING)
                 <form action="{{$routeApprove ?? "#"}}" method="post" class="d-inline mr-2">
                     @csrf
@@ -24,15 +27,15 @@ $role = Auth::user()->role ?? user()->role;
                 </form>
             @endif
         @endif
-    @elseif(in_array($role, App\Models\User::canEdit))
+    @elseif(in_array($role, canEdit))
         <a href="{{$routeEdit}}" class="btn btn-primary">Edit</a>
-    @elseif(in_array($role, App\Models\User::canDelete))
+    @elseif(in_array($role, canDelete))
         <form action="{{$routeDelete}}" method="post" class="d-inline">
             @csrf
             @method('DELETE')
             <button type="submit" class="btn btn-danger">Hapus</button>
         </form>
-    @elseif(in_array($role, App\Models\User::canDelete))
+    @elseif(in_array($role, canDelete))
         <form action="{{$routeApprove ?? "#"}}" method="post" class="d-inline mr-2">
             @csrf
             @method('PUT')
